c0ba1cefce15ad2cc5c1e7967a8085338368f3c9
Robert <robert_ny1989@mail.ru>
2021-08-12 10:03:49 +0300

    add getter and setter

--- src/main/java/com/git/demo/controller/model/Employee.java
+++ src/main/java/com/git/demo/controller/model/Employee.java
@@ -4,4 +4,28 @@ public class Employee {
     private String name;
     private String lastname;
     private Long age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }
